474aee004dfbba2e80bf554994a858fa1be0a4a6
Paola Caballero <paolisco2525@gmail.com>
2025-01-24 10:43:24 -0500

    Agregue página de portafolio y enlace hoja de vida

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './cv.html',
     }),
     new CopyPlugin({
       patterns: [
